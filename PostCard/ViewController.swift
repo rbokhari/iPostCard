6eabc2ad9d75def75b1353cb36c2dbc64f467c09
rahman <rahman@rahmans-Mac.local>
2014-11-08 21:46:04 +0400

    First Commit

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var txtName: UILabel!
+    @IBOutlet weak var txtMessage: UITextField!
+    @IBOutlet weak var cmdSend: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cmdSendMail(sender: UIButton) {
+        lblMessage.hidden = false
+        lblMessage.text = txtName.text
+        txtName.text = ""
+        
+        lblMessage.textColor = UIColor.redColor()
+        
+        txtName.resignFirstResponder()
+        cmdSend.setTitle("Mail Sent !", forState: UIControlState.Normal)
+        
+        
+    }
 
 }
 
